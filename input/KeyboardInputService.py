9162ddc2a8146b85080214ac80067056f7f0ef7d
Alexandre Fournier <as00paf@gmail.com>
2019-10-06 15:26:04 -0400

    Keyboard stuff

--- input/KeyboardInputService.py
+++ input/KeyboardInputService.py
@@ -1,4 +1,4 @@
-import keyboard
+from pynput import keyboard
 
 
 class KeyboardInputService:
@@ -9,20 +9,20 @@ class KeyboardInputService:
         self.power_service = power_service
         self.is_listening = False
         self.print_input = False
-        self.keyboard = keyboard.
-        self.keyboard.on_press(self.on_press)
-        self.keyboard.on_release(self.on_release)
+        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         self.listeners = {}
         self.logger.log(self.TAG, "KeyboardInputService instantiated")
 
     def start_listening(self, print_input=False):
         self.print_input = print_input
         self.is_listening = True
+        self.listener.start()
 
         self.logger.log(self.TAG, "Starting to listen")
 
     def stop_listening(self):
         self.is_listening = False
+        self.listener.stop()
         self.logger.log(self.TAG, "Listening stopped")
 
     def on_press(self, key):
