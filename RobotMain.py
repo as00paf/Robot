7e8bbdcc3e9be37c5c86e7ce91e8821e7bc4a656
Alexandre Fournier <as00paf@gmail.com>
2019-10-07 20:58:24 -0400

    webserver stuff

--- RobotMain.py
+++ RobotMain.py
@@ -39,7 +39,7 @@ class RobotMain:
 
         # Web services
         self.webapp = Flask(__name__)
-        self.webapp.run(host='192.168.1.105', port='8000', debug=True)
+        self.webapp.run(host='0.0.0.0', port='8000', debug=True)
 
         time.sleep(0.5)
         self.logger.log(self.TAG, "All services initialized", True)
@@ -71,6 +71,10 @@ class RobotMain:
         self.is_running = False
         self.charge_detector_service.stop_monitoring()
 
+
+@webapp.route('/')
+def index():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     main = RobotMain()
